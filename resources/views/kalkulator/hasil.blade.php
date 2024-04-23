f71011406c8e41e8147e76e6042c2de49623b71c
AfizaAnanta <anantaviza@gmail.com>
2024-04-23 10:30:50 +0700

    ukk

---
+++ resources/views/kalkulator/hasil.blade.php
@@ -0,0 +1,82 @@
+<div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="card">
+                    <div class="card-body">
+                    <div class="row">
+        <div class="col-md-6">
+           
+        </div>
+        <div class="row">
+        <div class="col-md-6">
+        <style>
+    h1 {
+  text-align: center;
+}
+body {
+    background-color: 	white;
+    outline: auto;
+    border: 2px outset black;
+    padding: 10%;
+ }
+form {
+  width: 400px;
+  margin: 0 auto; /* Center the form horizontally */
+  padding: 20px;
+  border: 1px solid black;
+  border-radius: 8px;
+  background-color: #9195F6;
+}
+input[type="submit"] {
+  padding: 10px 20px;
+  background-color: #0D9276;
+  color: white;
+  border: none;
+  border-radius: 6px;
+  cursor: pointer;
+}
+ </style>
+           
+        </div>
+    </div>
+                    <center>
+                        <h1>Kalkulator sederhana</h1>
+                        <div class="calculator">
+                            <form action="{{route('hitung.store')}}" method="post">
+                                @csrf
+                                <table border=1 width=400>
+
+                                <tr>
+                                <td colspan=2><input type="number" name="angka1" id=""><br></td>
+                                <td colspan=2 rowspan=2><input type="submit" value="reset" name="op"></td>
+                                </tr>
+                                 <tr>
+                                 <td colspan=2><input type="number" name="angka2" id=""><br></td>
+                                 </tr>
+                                 <tr>
+                            <td colspan=4>Hasil:{{$hasil}}</td>
+                           </tr>
+                                 <tr>
+                                    <td><input type="submit" value="+" name="op"></td>
+                                    <td><input type="submit" value="-" name="op"></td>
+                                    <td><input type="submit" value="*" name="op"></td>
+                                    <td><input type="submit" value="/" name="op"></td>
+                                 </tr>
+                            <br>
+                            <td colspan=4>
+                                Nama: Afiza Ananta <br>
+                                Kelas :XII RPL
+
+                            </form>
+
+                            </td>
+                            </table>
+                        </div>
+                        </center>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+   
---
+++ resources/views/kalkulator/index.blade.php
@@ -0,0 +1,82 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>fiza</title>
+</head>
+<body>
+<div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="card">
+                    <div class="card-body">
+
+
+        <style>
+    h1 {
+  text-align: center;
+}
+body {
+    background-color: 	white;
+    outline: auto;
+    border: 2px outset black;
+    padding: 10%;
+ }
+form {
+  width: 400px;
+  margin: 0 auto;
+  padding: 20px;
+  border: 1px solid black;
+  border-radius: 8px;
+  background-color:	#CD5C5C;
+}
+input[type="submit"] {
+  padding: 10px 20px;
+  background-color: #778899;
+  color: white;
+  border: none;
+  border-radius: 6px;
+  cursor: pointer;
+}
+ </style>
+                    <center>
+                        <h1>Kalkulator Sederhana</h1>
+                        <div class="calculator">
+                            <form action="{{route('hitung.store')}}" method="post">
+                                @csrf
+                                <table border=1 width=400>
+
+                                <tr>
+                                <td colspan=2><input type="number" name="angka1" id=""><br></td>
+                                <td colspan=2 rowspan=2><input type="submit" value="reset" name="op"></td>
+                                </tr>
+                                 <tr>
+                                 <td colspan=2><input type="number" name="angka2" id=""><br></td>
+                                 </tr>
+                                 <tr>
+                                    <td><input type="submit" value="+" name="op"></td>
+                                    <td><input type="submit" value="-" name="op"></td>
+                                    <td><input type="submit" value="*" name="op"></td>
+                                    <td><input type="submit" value="/" name="op"></td>
+                                 </tr>
+                            <br>
+                            <td colspan=4>
+                            Nama: Afiza Ananta <br>
+                                Kelas :XII RPL
+
+                            </form>
+                           
+                            </td>
+                            </table>
+                        </div>
+                        </center>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+   
+</body>
+</html>
